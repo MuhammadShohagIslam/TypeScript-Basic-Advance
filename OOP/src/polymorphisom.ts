011694e734abbe7485f0b6ff98d2c8d5f4fcab8c
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-05-05 20:54:09 +0300

    Polymorphisom

--- OOP/src/polymorphisom.ts
+++ OOP/src/polymorphisom.ts
@@ -1,17 +1,37 @@
+/*
+    *** Polymorphisom ***
+
+      => Poly -> অনেকগুলো (Many), Morphisam -> রূপ , so 
+      Polymorphisam means যে জিনিসটার অনেকগুলো রূপ থাকতে সেটাই 
+      Polymorphisam, A thing that has many forms or behaivehour
+      it's called Polymorphisam. 
+
+      => e.m: আমাদের আশেপাশে অনেক বহুরূপে মানুষ রয়েছে সেগুলো  
+      স্থানভেদে রূপ পরিবর্তন করে তাকে এগুলো হলো বহুরূপী. মানুষটা 
+      same কিন্তু স্থান কাল ভেদে ভিন্ন রূপ দেখায়., it's call
+
+      => যখন অনেকগুলো class থাকবে, ক্লাসের same মেথড, different 
+      different output দিবে, রুপ দেখাবে, সেটাই Polymorphisom.
+      When we have many class, same method of our class gives  
+      different output or different behaivehour, it's called 
+      Polymorphisam
+
+
+*/
 class Person {
-  takeNap(): void {
+  takeNap(): void { // same method
     console.log("I am sleeping 8 hours per day");
   }
 }
 
 class Student extends Person {
-  takeNap(): void {
+  takeNap(): void { // same method
     console.log(`I am sleeping 10 hours per day`);
   }
 }
 
 class Developer extends Person {
-  takeNap(): void {
+  takeNap(): void { // same method
     console.log(`I am sleeping 5 hours per day`);
   }
 }
@@ -23,12 +43,16 @@ function getNap(param: Person) {
 const person1 = new Person();
 const person2 = new Student();
 const person3 = new Developer();
-getNap(person1);
-getNap(person2);
-getNap(person3);
+
+// A thing that has many forms or behaivehour
+// it's called Polymorphisam. 
+getNap(person1); // output different
+getNap(person2); // output different
+getNap(person3); // output different
+
 
 class Shape {
-  getArea(): number {
+  getArea(): number { // same method
     return 0;
   }
 }
@@ -40,6 +64,7 @@ class Circle extends Shape {
     super();
     this.radius = radius;
   }
+  // same method
   getArea(): number {
     return Math.PI * this.radius * this.radius;
   }
@@ -53,6 +78,7 @@ class Rectangle extends Shape {
     this.height = height;
     this.width = width;
   }
+  // same method
   getArea(): number {
     return this.width * this.height;
   }
@@ -62,5 +88,7 @@ function getAreaOfShape(param: Shape) {
   console.log(param.getArea());
 }
 
-getAreaOfShape(new Circle(10));
-getAreaOfShape(new Rectangle(10, 12));
+// A thing that has many forms or behaivehour
+// it's called Polymorphisam. 
+getAreaOfShape(new Circle(10));  // output different
+getAreaOfShape(new Rectangle(10, 12)); // output different
