c33cc95747cbb7aaa86c847f7e3a5187c47c21e2
Md Jhohirul Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2023-06-02 18:42:39 +0300

    Create oop def

---
+++ OOP/src/theory.ts
@@ -0,0 +1,7 @@
+/*
+  *** What is OOP ***
+    => OOP is one kind of programming paradaigm which involves organize code into objects that interact with each other 
+        and also easy to maintable, readable.
+    => OOP is way write code with object pattern. Class or object is the fundamental block of OOP. We can make many object 
+       from one class.
+*/
