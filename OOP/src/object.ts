59a9f25e382d1a649168083f66910f52e99fcd0d
Md Jhohirul Islam <91692259+MuhammadShohagIslam@users.noreply.github.com>
2023-06-02 18:48:34 +0300

    Update object.ts

--- OOP/src/object.ts
+++ OOP/src/object.ts
@@ -1,4 +1,10 @@
 class Animal {
+  // name:string
+  // species:string
+  // sound:string
+  
+  // define shorcut way parameter properties with public
+  // *** thorugh parameter propeties, we can do together withour type decalre and initialize.
   constructor(
     public name: string,
     public species: string,
@@ -9,6 +15,8 @@ class Animal {
   }
 }
 
+// we can make many object from one class like from Animal  class to create two object such as dog, cat
+// which more clean and easy to maintable and readbale.
 const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
 const cat = new Animal("Persian", "cat", "meaw meaw");
 dog.makeSound();
